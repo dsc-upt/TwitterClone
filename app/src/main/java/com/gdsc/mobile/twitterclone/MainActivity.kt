f1810f02c31826c90506efa6e211c40e1cc818bd
Sorina <sorinapopaan@gmail.com>
2022-02-16 21:20:26 +0200

    Bottom-Navigation
    Started bottom navigation implementation

--- app/src/main/java/com/gdsc/mobile/twitterclone/MainActivity.kt
+++ app/src/main/java/com/gdsc/mobile/twitterclone/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.gdsc.mobile.twitterclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            .setupWithNavController(navController)
+        //TODO: tie destination to menu items
     }
+
+
 }

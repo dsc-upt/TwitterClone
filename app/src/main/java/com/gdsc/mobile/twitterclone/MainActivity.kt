c776484753f214b4944af39edc862f645549368f
Sorina <sorinapopaan@gmail.com>
2022-03-17 20:00:38 +0200

    Created navigation drawer

--- app/src/main/java/com/gdsc/mobile/twitterclone/MainActivity.kt
+++ app/src/main/java/com/gdsc/mobile/twitterclone/MainActivity.kt
@@ -2,32 +2,47 @@ package com.gdsc.mobile.twitterclone
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setSupportActionBar(findViewById(R.id.toolbar))
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        findViewById<BottomNavigationView>(R.id.bottom_navigation)
-            .setupWithNavController(navController)
+        navController = navHostFragment.navController
+        val navBottomView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navDrawerView = findViewById<NavigationView>(R.id.navigation_drawer)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        appBarConfig = AppBarConfiguration(setOf(R.id.homeFragment,R.id.searchFragment), drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfig)
+        navDrawerView.setupWithNavController(navController)
+        navBottomView.setupWithNavController(navController)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfig)
     }
 
 
